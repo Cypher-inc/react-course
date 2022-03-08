16e40ccf5e0dd121c3be9f9678fb5662ac0bcbe0
Cypher-inc <victorynorden@gmail.com>
2022-03-08 18:48:45 +0530

    date3

--- react-js/src1/components/Expenses/ExpenseDate.js
+++ react-js/src1/components/Expenses/ExpenseDate.js
@@ -5,6 +5,7 @@ const ExpenseDate = (props) => {
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
 
+  
   return (
     <div className='expense-date'>
       <div className='expense-date__month'>{month}</div>
