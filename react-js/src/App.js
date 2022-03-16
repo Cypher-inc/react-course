cc7b39d880ddc79a886fac079884acaa215cc8e9
Cypher-inc <victorynorden@gmail.com>
2022-03-16 11:56:18 +0530

    using useEffects

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 //app rerusn every time a state changes
 //side effects should not be put in app
@@ -10,9 +10,19 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {  const storedUserLoginInfo = localStorage.getItem("isLoggedIn");
+
+  if (storedUserLoginInfo === "1") {
+    setIsLoggedIn(true);
+    //Every time the set function is triggered the app component is triggered!
+    //CAUSES AN INFINTE LOOP, so we use useEffect
+  }}, []); //sets the dependencies
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    // localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
