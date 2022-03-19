e7c9d7789a5c82c8bb9e825deee5d0c11d26d53e
Cypher-inc <victorynorden@gmail.com>
2022-03-19 14:12:51 +0530

    modal logic1

--- react-js/src/comp/Modal.js
+++ react-js/src/comp/Modal.js
@@ -3,10 +3,11 @@ import { Modal, Button } from "react-bootstrap";
 import ModalBody from "./ModalBody";
 
 const ModalComp = (props) => {
-  //   const [show, setShow] = useState(false);
-
-  //   const handleClose = () => setShow(false);
-  //   const handleShow = () => setShow(true);
+  let totalDataVal
+  const modalToalValRec = (totalData) => {
+    console.log(totalData);
+    totalDataVal = totalData
+  };
 
   return (
     <>
@@ -19,13 +20,28 @@ const ModalComp = (props) => {
           <Modal.Title>Your Orders</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <ModalBody></ModalBody>
-          <ModalBody></ModalBody>
-          <ModalBody></ModalBody>
+          <ModalBody
+            dishName={"Sushi"}
+            dishCost={22.99}
+            dishQuantity={1}
+            modalToalVal={modalToalValRec}
+          ></ModalBody>
+          <ModalBody
+            dishName={"Schnitzel"}
+            dishCost={16.51}
+            dishQuantity={3}
+            modalToalVal={modalToalValRec}
+          ></ModalBody>
+          <ModalBody
+            dishName={"Barbeque Borgir"}
+            dishCost={12.99}
+            dishQuantity={2}
+            modalToalVal={modalToalValRec}
+          ></ModalBody>
 
           <div className="d-flex mt-3 modal-total">
             <h5>Total Amount</h5>
-            <span className="modal-total-cost">$55.87</span>
+            <span className="modal-total-cost">${totalDataVal}</span>
           </div>
         </Modal.Body>
 
--- react-js/src/comp/ModalBody.js
+++ react-js/src/comp/ModalBody.js
@@ -1,24 +1,66 @@
 import "./ModalBody.css";
+import { React, useState } from "react";
 
-const ModalBody = () => {
+const ModalBody = (props) => {
+  const [quantityVal, setQuantityVal] = useState(props.dishQuantity);
+  
+//   let quantityVal2;
+//   const decFunc = () => {
+//     let quantityVal1 = document.querySelector(".ModalQuantity").textContent;
+//     // console.log(quantityVal1[1]);
+//     quantityVal2 = Number(quantityVal1[1]);
+//     quantityVal2 += quantityVal2+
+//     console.log(quantityVal2);
+    
+//   };
+
+    const testQuantVal = (event) => {
+        console.log(event.target.value);
+    }
+
+  //Calc total function
+  let total = 0;
+  // let total1
+  const calcTotal = (x, y) => {
+    total += x * y;
+    // total1 = total
+    // console.log(total);
+  };
+  calcTotal(props.dishCost, props.dishQuantity);
+
+  function sendTotal() {
+    props.modalToalVal(total);
+  }
+  //   console.log(sendTotal);
+
   return (
-      <>
-    <div className="pt-2 pb-3 m-1 order-details">
-      <h5>Schnitzel</h5>
-      <div className="d-flex modal-info align-items-center">
-        <span className="ModalTotal">$16.5</span>
-        <span className="ModalQuantity">x2</span>
-        <div>
-          <button type="button" className="btn btn-outline-dark dark-btn1">
-            ➖
-          </button>
-          <button type="button" className="btn btn-outline-dark">
-            ➕
-          </button>
+    <>
+      <div className="pt-2 pb-3 m-1 order-details">
+        <h5>{props.dishName}</h5>
+        <div className="d-flex modal-info align-items-center">
+          <span className="ModalTotal">${props.dishCost}</span>
+          <span className="ModalQuantity" value={quantityVal}
+          onLoad={testQuantVal}>
+            x{props.dishQuantity}
+          </span>
+          <div>
+            <button
+              type="button"
+              className="btn btn-outline-dark dark-btn1"
+            //   onClick={decFunc}
+            >
+              ➖
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-dark"
+              onClick={sendTotal}
+            >
+              ➕
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-
     </>
   );
 };
