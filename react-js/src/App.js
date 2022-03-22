3ceafcb1ad0ebc85b9f2224dc00b16a639780f35
Cypher-inc <victorynorden@gmail.com>
2022-03-22 14:00:48 +0530

    practice newTry

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,36 +1,19 @@
-import {React, useState} from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import ModalComp from "./comp/Modal";
-import NavBar from "./comp/NavBar";
-import OrderList from "./comp/OrderList";
-import "./index.css";
-import CartProvider from "./store/CartProvider";
+import { Fragment } from 'react';
 
-const App = () => {
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
-  const [userData , setUserData] = useState('')
-
-  const checkPassFunc = (data) => {
-    // console.log(data);
-    
-    setUserData((prevData) => {
-      return [data, ...prevData];
-    });
-  }
-
+function App() {
   return (
-    <CartProvider>
-      <NavBar passVal={userData}></NavBar>
-
-      <div className="container d-flex h-100 main">
-        <div className="row justify-content-center align-self-center">
-          <h1 className="text-center mt-4 bg-light">Hello</h1>
-
-          <OrderList dataPassFunc1={checkPassFunc}></OrderList>
-        </div>
-      </div>
-    </CartProvider>
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
-};
+}
 
 export default App;
