0215e251e2d5bf610b3c33e548c91f274838a3e3
Cypher-inc <victorynorden@gmail.com>
2022-04-02 17:26:09 +0530

    forward_useRef

--- react-js/src/components/inputComp.js
+++ react-js/src/components/inputComp.js
@@ -1,24 +1,34 @@
-import React from "react";
+import {React, useEffect, useRef, useImperativeHandle} from "react";
 
 const InputComp = (props) => {
     console.log('Hello World');
-    
+    const inputRef = useRef()
+
+    // useEffect(()=>{
+    //     inputRef.current.focus()
+    // },[])
+
+    const activate = () => {
+      inputRef.current.focus()
+    }
+
    return(
        <><div
        className={`${props.pClass.control} ${
-         props.pEmail.isValid === false ? props.pClass.invalid : ""
+         props.pIsValid === false ? props.pClass.invalid : ""
        }`}
      >
-       <label htmlFor="email">E-Mail</label>
+       <label htmlFor={props.pName}>{props.pName}</label>
        <input
-         type="email"
-         id="email"
-         value={props.pEmail.value}
-         onChange={props.pEmailChange}
-         onBlur={props.pEmailValid}
+         type={props.pName}
+         id={props.pName}
+         value={props.pValue.value}
+         onChange={props.pChange}
+         onBlur={props.pOnBlur}
+         ref={inputRef}
        />
      </div>
-     <div
+     {/* <div
        className={`${props.pClass.control} ${
          props.pPass.isValid === false ? props.pClass.invalid : ""
        }`}
@@ -31,7 +41,8 @@ const InputComp = (props) => {
          onChange={props.pPassChange}
          onBlur={props.pPassValid}
        />
-     </div></>
+     </div> */}
+     </>
    )
 }
 
